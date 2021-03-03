a2c206f6fa660fedcff28226cfc0be146bc05704
Nick Johnson <Johnsn0913@gmail.com>
2021-03-03 14:33:04 -0600

    added table data through ajax on app.js

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 "use strict"
+$(document).ready(function(){
+    $.ajax({
+        url: 'https://localhost:44325/api/movie',
+        dataType: "json",
+        type: "get",
+        success: function(data){
+            $(".movieData").html('');
+        },
+        error: function(errorThrown){
+            console.log(errorThrown);
+        }
+    })
+    .then(function(data){
+        $.each(data, function(index, value){
+            $(".movieData").append(
+                "<tr>" + 
+                    "<td>" + value.movieId + "</td>" +
+                    "<td>" + value.title + "</td>" +
+                    "<td>" + value.director + "</td>" +
+                    "<td>" + value.genre + "</td>" +
+                "</tr>"
+            );
+        });
+    });
+});
