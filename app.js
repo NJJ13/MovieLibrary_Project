2ef4b0bb44c99f94d2a0550b3fc9f011c8f8ca99
Nick Johnson <Johnsn0913@gmail.com>
2021-03-04 15:31:48 -0600

    processChange Method Added

--- app.js
+++ app.js
@@ -44,27 +44,47 @@ function movieDetails(id){
             "</tr>")}
         });
     });
-    $.get('https://localhost:44325/api/movie/', function(data){
-        data.map(function(el){
-            if(id === el.movieId){
-            $(".editInfo").html(
-                "<h3> Update Information</h3>" +
-                "<div class='form-group'>" +
-                "<label class='col-form-label' for='inputDefault'>Title</label>" +
-                "<input type='text' class='form-control' placeholder='Updated Title' id='inputDefault'>"+
-                "<button type='button'" + "class='btn btn-primary btn-sm'>" + "Save Changes" + "" + "</button>" +
-            "</div>" +
-            "<div class='form-group'>" +
-                "<label class='col-form-label' for='inputDefault'>Director</label>" +
-                "<input type='text' class='form-control' placeholder='Updated Director' id='inputDefault'>"+
-                "<button type='button'" + "class='btn btn-primary btn-sm'>" + "Save Changes" + "" + "</button>" +
-            "</div>" +
-            "<div class='form-group'>" +
-                "<label class='col-form-label' for='inputDefault'>Genre</label>" +
-                "<input type='text' class='form-control' placeholder='Updated Genre' id='inputDefault'>"+
-                "<button type='button'" + "class='btn btn-primary btn-sm'>" + "Save Changes" + "" + "</button>" +
-            "</div>" 
-            )}
-        });
-    });
+    $.get('https://localhost:44325/api/movie/'+id, function(data){
+        $(".editInfo").html(
+            `<h3> Update Information</h3>
+            <div class='form-group'>
+            <label class='col-form-label' for='title'>Title</label>
+            <input type='text' class='form-control' value='${data.title}' id='title'>
+        </div>
+        <div class='form-group'>
+            <label class='col-form-label' for='director'>Director</label>
+            <input type='text' class='form-control' value='${data.director}' id='director'>
+        </div>
+        <div class='form-group'>
+            <label class='col-form-label' for='genre'>Genre</label>
+            <input type='text' class='form-control' value='${data.genre}' id='genre'>
+        </div>
+        <button type='button' onclick='processChange();' class='btn btn-primary btn-sm'>Save Changes</button>` 
+        )}
+    );
 };
+
+function processChange($){
+    function processForm( e ){
+        var dict = {
+            Title : this["title"].value,
+            Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function(data){
+                $('#response pre').html( data );
+            },
+            error: function(errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+        e.preventDefault();
+    }
+        $(".editInfo").submit( processForm );
+    }(jQuery);
