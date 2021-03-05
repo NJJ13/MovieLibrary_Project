021a283597c016fb32939c267bc7cd0c494bcf89
Nick Johnson <Johnsn0913@gmail.com>
2021-03-05 10:52:02 -0600

    updated add function to populate on document ready. Created assignHandler, createTables, and returnToHome functions

--- app.js
+++ app.js
@@ -6,25 +6,13 @@ $(document).ready(function(){
         type: "get",
         success: function(data){
             $('.movieData').html('');
+            createTables(data);
+            assignHandler();
         },
         error: function(errorThrown){
             console.log(errorThrown);
         }
     })
-    .then(function(data){
-        $(".tableHead").append(
-            "<th scope='col'>Title</th>" , 
-            "<th scope='col'></th>"
-        )
-        $.each(data, function(index, value){
-            $('.movieData').append(
-                "<tr>" + 
-                    "<td>" + value.title + "</td>" +
-                    "<td>" + "<button type='button'" + "onclick='movieDetails(" + value.movieId + ");'" + "class='btn btn-primary btn-lg'>" + "Details" + "" + "</button>" +
-                "</tr>"
-            );
-        });
-    });
 });
 
 function movieDetails(id){
@@ -33,6 +21,7 @@ function movieDetails(id){
         "<th scope='col'>Director</th>"+ 
         "<th scope='col'>Genre</th>"
     )
+    $("#header").html("<h1>Details</h1>");
     $.get('https://localhost:44325/api/movie/', function(data){
         data.map(function(el){
             if(id === el.movieId){
@@ -61,8 +50,11 @@ function movieDetails(id){
                 <input type='text' class='form-control' value='${data.genre}' id='genre'>
             </div>
         </div>
-        <button type='button' onclick='processChange($('#updateForm'));' class='btn btn-primary btn-sm'>Save Changes</button>` 
+        <button type='button' onclick='processChange($('#updateForm'));' class='btn btn-primary btn-sm'>Save Changes</button> <br>` 
         )}
+    );
+    $("#response").append(
+        `<button type='button' onclick='returnToHome();' class='btn btn-primary btn-large'>Return to Library</button>`
     );
 };
 
@@ -76,7 +68,7 @@ function processChange($){
         $.ajax({
             url: 'https://localhost:44325/api/movie',
             dataType: 'json',
-            type: 'post',
+            type: 'put',
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function(data){
@@ -89,9 +81,32 @@ function processChange($){
         e.preventDefault();
     }
         $(".editInfo").submit( processForm );
-    }(jQuery);
+}(jQuery);
 
-(function($){
+function createTables(data){
+    $("#header").append(
+    `<label>
+        Title:<input type="text" id="title">
+        Director:<input type="text" id="director">
+        Genre:<input type="text" id="genre">
+    </label>
+    <button class='btn btn-primary' type='submit'  id="add">Add</button>`
+    )
+    $(".tableHead").append(
+        "<th scope='col'>Title</th>" , 
+        "<th scope='col'></th>"
+    )
+    $.each(data, function(index, value){
+        $('.movieData').append(
+            "<tr>" + 
+                "<td>" + value.title + "</td>" +
+                "<td>" + "<button type='button'" + "onclick='movieDetails(" + value.movieId + ");'" + "class='btn btn-primary btn-lg'>" + "Details" + "" + "</button>" +
+            "</tr>"
+        );
+    });
+};
+
+function assignHandler(){
     $('#add').on('click', function(){
         var newMovie = {
             title : $('#title').val(),
@@ -105,11 +120,31 @@ function processChange($){
             contentType : 'application/json',
             data: JSON.stringify(newMovie),
             success: function(movie){
-               $('.movieData').append('<tr><td>' + movie.title + '</td>' + `<td><button type='button' onclick='movieDetails(${movie.movieId});' class='btn btn-primary btn-lg'>Details</button>` + '</td></tr>'); 
+                $('.movieData').append('<tr><td>' + movie.title + '</td>' + `<td><button type='button' onclick='movieDetails(${movie.movieId});' class='btn btn-primary btn-lg'>Details</button>` + '</td></tr>'); 
             },
             error: function(errorThrown ){
                 console.log( errorThrown );
             }
         });
     });
-})(jQuery);
+}
+
+function returnToHome(){
+    $("#header").html("<h1> Movie Library </h1>");
+    $(".editInfo").html("");
+    $("#response").html("");
+    $(".tableHead").html("");
+    $.ajax({
+        url: 'https://localhost:44325/api/movie',
+        dataType: "json",
+        type: "get",
+        success: function(data){
+            $('.movieData').html('');
+            createTables(data);
+            assignHandler();
+        },
+        error: function(errorThrown){
+            console.log(errorThrown);
+        }
+    })
+}
